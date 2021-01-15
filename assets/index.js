70b3d5ec612047fda1094bcb9f53245bea34328d
Scott Hoisington <hoyzington@gmail.com>
2021-01-15 07:45:03 -0500

    edit svg; add choosing reset to mapMsas; improve MSA list appearance; refactor & remove unused code

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,17 @@
 const endPoint = 'http://localhost:3000/api/v1/metro-areas'
 
 function mapMsas(json) {
-  console.log('Hello??')
   const svgObj = document.getElementById('map')
-  if (svgObj) {
-    console.log('got svg')
-  } else {
-    console.log("didn't get svg")
-  }
-  // const prev_msas = document.getElementsByClassName('chosen')
-  // if (prev_msas.length > 0) {
-  //   for (const msa of prev_msas) {
-  //     msa.className = 'prev_chosen'
-  //   }
-  // }
   const chosen = document.getElementById('chosen-msas')
   const not_chosen = document.getElementById('not-chosen-msas')
+  if (chosen.children.length > 0) {
+    for (const msa of chosen) {
+      not_chosen.appendChild(msa)
+    }
+  }
   for (const msa of json) {
-    let loc = svgObj.getElementById(`MSA_${msa.msa_code}`)
+    let loc = svgObj.getElementById(msa.msa_code)
     if (loc) {
-      console.log(loc)
       chosen.appendChild(loc)
     } else {
       console.log(`${msa.msa_code} ${msa.name} was NOT found`)
@@ -29,14 +21,12 @@ function mapMsas(json) {
 
 function makeList(json) {
   for (const msa of json) {
-    let list = document.getElementById('metro-list')
-    let div = document.createElement('div')
-    let card = document.createElement('div')
-    card.className = 'list-item'
-    card.id = msa.msa_code
-    card.innerHTML = `<b>${msa.name}</b> (${msa.states})`
-    div.appendChild(card)
-    list.appendChild(div)
+    let ul = document.getElementById('metro-list')
+    let li = document.createElement('li')
+    li.className = 'list-item'
+    li.id = msa.msa_code
+    li.innerHTML = `<b>${msa.name}</b> (${msa.states})`
+    ul.appendChild(li)
   }
   mapMsas(json)
 }
@@ -45,7 +35,6 @@ function getMsas() {
   fetch(endPoint)
     .then(res => res.json())
     .then(json => makeList(json))
-    // .then(json => mapMsas(json))
 }
 
 getMsas()
